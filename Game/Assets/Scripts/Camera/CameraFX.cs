2a2c59f16feed5eaf96f92aed80a9d7bacb95e0e
Conflei <paramaplexd@gmail.com>
2014-11-22 05:00:17 -0430

    Camera FX 3

--- Game/Assets/Scripts/Camera/CameraFX.cs
+++ Game/Assets/Scripts/Camera/CameraFX.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class CameraFX : Singleton<CameraFX> {
 
+	public bool testingFX = false;
+
 	public enum MyColors
 		{
 		Blue,
@@ -28,6 +30,16 @@ public class CameraFX : Singleton<CameraFX> {
 	public Sprite whiteNoise;
 	public Sprite defaultSprite;
 
+	public float zoomSpeed = 1f;
+
+	private bool shaking = false;
+	private Vector3 originalPosition;
+
+	private Vector3 ShakeRange = new Vector3(1,1,1);
+	private float ShakeTimer = 0f;
+	private const float ShakeTime = 0.75f;
+	private float ShakeSpeed = 50f;
+
 
 	// Use this for initialization
 	void Start () {
@@ -36,17 +48,40 @@ public class CameraFX : Singleton<CameraFX> {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey(KeyCode.O))FadeIn(whiteColor, 1f);
-		if(Input.GetKey(KeyCode.P))FadeOut(1f);
+		if (testingFX) {
+						if (Input.GetKey (KeyCode.O))
+								FadeIn (whiteColor, 1f);
+						if (Input.GetKey (KeyCode.P))
+								FadeOut (1f);
 
-		if(Input.GetKey(KeyCode.K))FadeIn(whiteColor, .2f);
-		if(Input.GetKey(KeyCode.L))FadeOut(.2f);
+						if (Input.GetKey (KeyCode.K))
+								Camera.main.orthographicSize += Time.deltaTime * zoomSpeed;
+						if (Input.GetKey (KeyCode.L))
+								Camera.main.orthographicSize -= Time.deltaTime * zoomSpeed;
 
-		if(Input.GetKey(KeyCode.N))FadeIn(whiteColor, .05f);
-		if(Input.GetKey(KeyCode.M))FadeOut(.05f);
+						if (Input.GetKey (KeyCode.N))
+								FadeIn (whiteColor, .05f);
+						if (Input.GetKey (KeyCode.M))
+								FadeOut (.05f);
 
-		if (Input.GetKey (KeyCode.U))
-						WhiteNoise (2f);
+						if (Input.GetKey (KeyCode.U))
+								WhiteNoise (2f);
+
+						if(Input.GetKey(KeyCode.C)) CameraShake();
+
+			if(shaking){
+				if(ShakeTimer > ShakeTime*Time.timeScale){
+					shaking = false;
+					Camera.main.transform.position = new Vector3(originalPosition.x, originalPosition.y,
+					                                             originalPosition.z);
+				}else{
+					ShakeTimer += Time.deltaTime;
+					Camera.main.transform.position = originalPosition + Vector3.Scale(SmoothRandom.GetVector2(ShakeSpeed--), ShakeRange);
+					ShakeSpeed *= -1;
+					ShakeRange = new Vector3(ShakeRange.x*-1, ShakeRange.y);
+				}
+			}
+		}
 	}
 
 	public void FadeIn(Color color, float time)
@@ -116,5 +151,13 @@ public class CameraFX : Singleton<CameraFX> {
 			yield return new WaitForSeconds (time/12f);	
 		}
 	}
+
+	public void CameraShake(){
+		shaking = true;
+		ShakeTimer = 0f;
+		originalPosition = Camera.main.transform.position;
+	}
+
+
 
 }
