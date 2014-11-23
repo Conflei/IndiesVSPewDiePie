88926ce4a7552f83d56e36374f5400ce82c8a413
Conflei <paramaplexd@gmail.com>
2014-11-23 13:41:07 -0430

    Felix nunca hce commit

---
+++ Game/Assets/Scripts/Characters/EndPoint.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndPoint : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void FirstCutscene(){
+		StartCoroutine (FCWorker ());
+	}
+
+	public IEnumerator FCWorker(){
+		Camera.main.GetComponent<CameraFollow> ().enabled = false;
+		yield return null;
+	}
+}
